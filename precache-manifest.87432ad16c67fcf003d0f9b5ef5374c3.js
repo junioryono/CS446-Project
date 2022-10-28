c67a628675977c7f72dbf3095481708fbcd917e8
junioryono <junioryono@users.noreply.github.com>
2022-10-28 23:39:48 +0000

    deploy: 53d96cc91f48b67f065fd89f46eed56974116d3e

---
+++ precache-manifest.87432ad16c67fcf003d0f9b5ef5374c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf57374528164a5bcd5f886966c4ff9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47cde07b2d6f77ce9b10",
+    "url": "./static/css/2.90da6880.chunk.css"
+  },
+  {
+    "revision": "2e5050cf58f373940569",
+    "url": "./static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "47cde07b2d6f77ce9b10",
+    "url": "./static/js/2.b81e05a8.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "./static/js/2.b81e05a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e5050cf58f373940569",
+    "url": "./static/js/main.9dc110da.chunk.js"
+  },
+  {
+    "revision": "0420f1b6bf582a7efde8",
+    "url": "./static/js/runtime-main.8e3eea1f.js"
+  }
+]);
